c614edb084da865a10007892695b183d662a7591
Tanishq Dubey <tanishq.dubey@gmail.com>
2019-11-16 17:12:45 -0500

    fix setup desc type

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def readme():
         return f.read()
 
 setup(name='harbormaster',
-      version='0.2',
+      version='0.2.1',
       description='automating docker remote host forwarding',
       long_description=readme(),
       long_description_content_type="text/markdown",
